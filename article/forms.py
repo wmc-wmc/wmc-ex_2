401144ef70e182fe1a151ea875aad9dc6561267b
cookiebus <ngncmh@gmail.com>
2020-04-01 16:04:45 +0800

    Update

---
+++ article/forms.py
@@ -0,0 +1,10 @@
+from django import forms
+from article.models import Text
+
+
+class ArticleForm(forms.ModelForm):
+
+	class Meta:
+		model = Text
+		fields = ['title', 'url', 'image', 'text', 'subject']
+
